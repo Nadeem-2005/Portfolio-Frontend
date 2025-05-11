9b39a906fc4c130e99f09b311588146a1232dfc4
Nadeem-2005 <mr.maestro002@gmail.com>
2025-05-11 17:30:00 +0530

    Implement successful submission prompt

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
 import ShinyText from "./Animations/ShinyText/ShinyText";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/Components/ui/alert-dialog"
+
 
 function Form() {
     const [formData, setFormData] = useState({
@@ -22,80 +34,98 @@ function Form() {
             const response = await axios.post('https://portfolio-backend-q1tq.onrender.com/send-mail', formData);
             console.log(response.data);
         } catch (error) {
-            console.error(error);
+            console.log(error);
         }
     };
 
     return (
-        <form onSubmit={submission} className="rounded-lg shadow-lg max-w-md mx-auto space-y-6">
-            <h2 className="text-2xl text-center text-white space-grotesk">Let's Connect</h2>
+        <>
 
-            <div className="flex flex-col space-y-2">
-                <label className="text-white" htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    id="name"
-                    className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
-                    required
-                />
-            </div>
+            <form onSubmit={submission} className="rounded-lg shadow-lg max-w-md mx-auto space-y-6">
+                <h2 className="text-2xl text-center text-white space-grotesk">Let's Connect</h2>
 
-            <div className="flex flex-col space-y-2">
-                <label className="text-white" htmlFor="email">E-mail</label>
-                <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    id="email"
-                    className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
-                    required
-                />
-            </div>
-
-            <div className="flex flex-col space-y-2">
-                <label className="text-white" htmlFor="phone">Phone Number</label>
-                <input
-                    type="tel"
-                    name="phone"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    id="phone"
-                    className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
-                    required
-                />
-            </div>
-
-            <div className="flex flex-col space-y-2">
-                <label className="text-white" htmlFor="message">Message</label>
-                <textarea
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    id="message"
-                    rows="4"
-                    className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
-                    required
-                ></textarea>
-            </div>
-
-            <div className="flex justify-center">
-                <button
-                    type="submit"
-                    className="text-white py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-white-500 cursor-pointer active:scale-95 transition-all duration-500  hover:bg-[rgb(34,34,34)] border border-[rgb(53,53,53)]"
-                >
-                    <ShinyText
-                        text="Submit"
-                        disabled={false}
-                        speed={3}
-                        className="text-3xl "
+                <div className="flex flex-col space-y-2">
+                    <label className="text-white" htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        id="name"
+                        className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
+                        required
                     />
-                </button>
-            </div>
-        </form>
+                </div>
+
+                <div className="flex flex-col space-y-2">
+                    <label className="text-white" htmlFor="email">E-mail</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        id="email"
+                        className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
+                        required
+                    />
+                </div>
+
+                <div className="flex flex-col space-y-2">
+                    <label className="text-white" htmlFor="phone">Phone Number</label>
+                    <input
+                        type="tel"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        id="phone"
+                        className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
+                        required
+                    />
+                </div>
+
+                <div className="flex flex-col space-y-2">
+                    <label className="text-white" htmlFor="message">Message</label>
+                    <textarea
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        id="message"
+                        rows="4"
+                        className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-white"
+                        required
+                    ></textarea>
+                </div>
+
+                <div className="flex justify-center">
+                    {/* <button
+                        type="submit"
+                        className="text-white py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-white-500 cursor-pointer active:scale-95 transition-all duration-500  hover:bg-[rgb(34,34,34)] border border-[rgb(53,53,53)]"
+                    >
+                        <ShinyText
+                            text="Submit"
+                            disabled={false}
+                            speed={3}
+                            className="text-3xl "
+                        />
+                    </button> */}
+                    <AlertDialog >
+                        <AlertDialogTrigger className="text-white py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-white-500 cursor-pointer active:scale-95 transition-all duration-500  hover:bg-[rgb(34,34,34)] border border-[rgb(53,53,53)]" type="submit">Submit</AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Message sent</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    Thank you for contacting me 🙏🏻. I will get back to you soon.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                {/* <AlertDialogCancel>Cancel</AlertDialogCancel> */}
+                                <AlertDialogAction type="submit">Continue</AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                </div>
+            </form>
+        </>
     );
 }
 

b489bbd871e8faa0a9fd5d8706c53db4e19a845d
Nadeem-2005 <mr.maestro002@gmail.com>
2025-09-05 19:15:15 +0530

    navbar change v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import PillNav from "./Components/Animations/PillNav/PillNav";
 import Skills from "./Components/Skills";
 import Projects from "./Components/Projects";
 import Experience from "./Components/Experience";
@@ -9,7 +10,23 @@ import Footer from "./Components/Footer";
 function App() {
   return (
     <div>
-      <Navbar />
+      <PillNav
+        logo={null}  // your logo path
+        items={[
+          { href: "#about", label: "About" },
+          { href: "#Skill", label: "Skills" },
+          { href: "#Projects", label: "Projects" },
+          { href: "#Experience", label: "Experience" },
+          { href: "#Contact", label: "Contact" },
+        ]}
+        activeHref="/"  // optional, to highlight the active section
+        baseColor="transparent"  // navbar background
+        pillColor="#f3f4f6"       // pill background
+        pillTextColor="#000000"   // text color
+        hoveredPillTextColor="#1a1a1a"
+        initialLoadAnimation="true"
+        ease="power2.easeOut"
+      />
       <Hero />
       <Skills />
       <Projects />
